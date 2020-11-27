9e2be5b33c01bdb46574c278b1ed2abc0eb3f2b4
Jakob Chill <jakobchill@gmx.de>
2020-11-27 19:41:01 +0100

    Add basic styling

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,26 @@ enum timeunit {
 
 class RgbTime {
   constructor(public root: HTMLElement){
+    //calling backgroundChange() once, so the inital-background, doesnt have to wait for the interval
     this.backgroundChange();
     this.builder();
     this.interval();
   }
 
-  timeToColor(timeNumber: number): number {
-    const value = Math.floor(Date.now() / timeNumber) % 510;
-    return value > 255 ? 255 - (value - 255) : value;
+  timeToColorStr(timeNumber: number): String {
+    let value = Math.floor(Date.now() / timeNumber) % 510;
+    value = value > 255 ? 255 - (value - 255) : value;
+    return value.toString().padStart(3, "0");
   }
 
-  get red(): number {
-    return this.timeToColor(timeunit.hour);
+  get red(): String {
+    return this.timeToColorStr(timeunit.hour);
   }
-  get green(): number {
-    return this.timeToColor(timeunit.min);
+  get green(): String {
+    return this.timeToColorStr(timeunit.min);
   }
-  get blue(): number {
-    return this.timeToColor(timeunit.sec);
+  get blue(): String {
+    return this.timeToColorStr(timeunit.sec);
   }
 
   backgroundChange(): void {
@@ -33,17 +35,17 @@ class RgbTime {
   builder(): void {
     this.root.innerHTML = `
       <div id="timeContainer">
-        <div id="timeDisplay">
-          RGB(${this.red}, ${this.green}, ${this.blue})
+        <div id="timeDisplay">#
         <div>
       </div>
       `
+    this.updateDisplay();
   }
 
   updateDisplay(): void {
     const timeDisplay = document.getElementById("timeDisplay");
     if (timeDisplay) {
-      timeDisplay.innerHTML = `RGB(${this.red}, ${this.green}, ${this.blue})`;
+      timeDisplay.innerHTML = `RGB(${this.red},${this.green},${this.blue})`;
     }
   }
 
