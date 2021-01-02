0bed0885815470f70408ab63f0e1b2e54d7efe48
Jakob Chill <jakobchill@gmx.de>
2021-01-02 20:44:30 +0100

    Reorganize scss

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import { HexButton } from "./HexButton";
 import { RGBClock } from "./RGBClock";
 import { RGBTimeGenerator } from "./RGBTimeGenerator";
+
 import "./css/main.scss";
+import { HideUI } from "./HideUI";
 
 const main = (
   clockfaceID: string,
   backgroundID: string,
-  buttonID: string
+  hexButtonID: string
 ): void => {
   const clockface = document.getElementById(clockfaceID);
   const background = document.getElementById(backgroundID);
-  const button = document.getElementById(buttonID);
+  const hexButton = document.getElementById(hexButtonID);
 
   const generator = new RGBTimeGenerator();
 
@@ -20,13 +22,13 @@ const main = (
     throw new Error(`No element with id ${clockface} found!`);
   }
 
-  if (button) {
-    new HexButton(button, generator);
+  if (hexButton) {
+    new HexButton(hexButton, generator);
   } else {
     throw new Error("Button with id testbutton not found");
   }
 };
 
-main("clockface", "background", "hexbutton");
+main("clocktime", "background", "hexbutton");
 
 // Funfact: While coding I witnessed RGB(0,0,0) on the 27.11.2020 at 19:00 GMT+1
